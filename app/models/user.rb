c41df3b8b5eb60fbf26865861f0e362a4a3c1178
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-04-15 19:08:47 +0200

    Ruby API that will be fetched to React

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_secure_password
+  # Other validations and methods for your User model
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
+
+  scope "users" do
+    post '/create', to: 'users#create'
+
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
